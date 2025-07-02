9312522bd86fd4f1d9144703dd3b36d230a16fd4
Essam Mobarez <mobarezessam555@gmail.com>
2025-07-02 20:56:12 +0000

    change also this icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -12,13 +12,11 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M6 22V4a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v18" />
-      <path d="M6 12H4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h2" />
-      <path d="M18 9h2a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2h-2" />
-      <path d="M10 6h4" />
-      <path d="M10 10h4" />
-      <path d="M10 14h4" />
-      <path d="M10 18h4" />
+      <path d="M12 22v-5" />
+      <path d="M20 22v-5" />
+      <path d="M4 22v-5" />
+      <path d="M15.13 2.69a2 2 0 0 0-2.26 0l-4 2.05a2 2 0 0 0-1.13 1.76V17a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V6.5a2 2 0 0 0-1.13-1.76l-4-2.05Z" />
+      <path d="M8 19h8" />
     </svg>
   );
 }
