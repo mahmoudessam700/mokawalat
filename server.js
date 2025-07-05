b4b012a3534de46e30620b935f5506e01845e5e0
Essam Mobarez <mobarezessam555@gmail.com>
2025-07-05 13:30:59 +0000

    why it gives me this error?

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ const { parse } = require('url');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
-const hostname = 'localhost';
+// Use '0.0.0.0' to accept connections on all network interfaces, which is necessary for many hosting environments.
+const hostname = '0.0.0.0'; 
 // The hosting provider (like cPanel) will set the PORT environment variable.
 const port = process.env.PORT || 3000; 
 
-const app = next({ dev, hostname, port });
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
@@ -23,8 +24,8 @@ app.prepare().then(() => {
       res.statusCode = 500;
       res.end('internal server error');
     }
-  }).listen(port, (err) => {
+  }).listen(port, hostname, (err) => {
     if (err) throw err;
-    console.log(`> Ready on http://${hostname}:${port}`);
+    console.log(`> Ready on http://localhost:${port}`);
   });
 });
